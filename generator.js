7f0d1a5157766deba0b1e2c188b716fca3e152b2
larsencs <quietus77@gmail.com>
2022-02-18 08:30:01 -0600

    Renamed .js files. Added HTML elements. Added code to main.js to modify DOM. Added comments

--- generator.js
+++ generator.js
@@ -1,40 +1,35 @@
-const coloredReindeerBuilder = function () {
-    const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-
-    const coloredReindeer = []
-    // Write a for loop that looks at each reindeer
-    for(let i = 0; i<reindeer.length; i++){
-        console.log(reindeer[i])
-        // console.log(reindeerFactory(reindeer[i]), generateColors())
-    }
-        // Invoke factory function to create reindeer object
-
-        // Put new reindeer object in coloredReindeer array
-
-
-    // Return coloredReindeer array
-
-    // return coloredReindeer
-}
+//factory function to create objects and assign a name and color property
 const reindeerFactory = (name, color) =>{
-    reindeerObject = {}
-    reinderObject.name = name
+    const reindeerObject = {}
+    reindeerObject.name = name
     reindeerObject.color = color
 
     return reindeerObject
 }
+//generator function that assigns colors in the order of the index each time it is called
 const generateColors = function* (){
     const colors = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
     let i = 0
-    return colors[i]
-    i++
+    while(i < colors.length){
+        yield colors[i]
+        i+=1
+    }
 }
+//calls the generator function, as calling the generator function directly results in an object of "Generator"
+const colorFactory = generateColors()
 
-coloredReindeerBuilder()
+//builds an array of colored Reindeer using both the factory and generator functions
+const coloredReindeerBuilder = function () {
+    const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
+    const coloredReindeer = []
+    
+    for(let i = 0; i<reindeer.length; i++){
+        const name = reindeer[i]
+        const color = colorFactory.next().value
+        const reindeerObj  = reindeerFactory(name, color)
+        coloredReindeer.push(reindeerObj)
+    }
 
-
-
-
-// ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
-
+    return coloredReindeer
+}
