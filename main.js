87d08b043ea5535c5f38f7709eb7cf191a15fd48
larsencs <quietus77@gmail.com>
2022-02-18 06:10:16 -0600

    Generator function now works as intended

--- main.js
+++ main.js
@@ -8,21 +8,23 @@ const reindeerFactory = (name, color) =>{
 const generateColors = function* (){
     const colors = ["Blue", "Red", "Orange", "Purple", "Goldenrod", "Aquamarine", "Olive", "Azure", "Fuchsia", "Chocolate", "Salmon", "Amaranth"]
     let i = 0
-    return colors[i]
-    i++
+    while(i < colors.length){
+        yield colors[i]
+        i+=1
+    }
 }
+const colorFactory = generateColors()
 const coloredReindeerBuilder = function () {
     const reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 
     const coloredReindeer = []
     // Write a for loop that looks at each reindeer
     for(let i = 0; i<reindeer.length; i++){
-        const reindeerObj = (reindeerFactory(reindeer[i]), generateColors())
+        const name = reindeer[i]
+        const color = colorFactory.next().value
+        const reindeerObj  = reindeerFactory(name, color)
         coloredReindeer.push(reindeerObj)
     }
-        // Invoke factory function to create reindeer object
-
-        // Put new reindeer object in coloredReindeer array
 
 
     // Return coloredReindeer array
@@ -30,9 +32,7 @@ const coloredReindeerBuilder = function () {
     return coloredReindeer
 }
 
-
 console.log(coloredReindeerBuilder())
-
 
 
 
